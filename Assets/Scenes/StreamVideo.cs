6a91102d1ceb161b5b1c32f2643a165bf4c96493
Evan Gofourth <evangofourth99@gmail.com>
2020-01-29 19:41:58 -0600

    Initial commit, some basic UI

---
+++ Assets/Scenes/StreamVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    
+
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitforseconds = new WaitForSeconds(1);
+        while(!videoPlayer.isPrepared)
+        {
+            yield return waitforseconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }
+}
